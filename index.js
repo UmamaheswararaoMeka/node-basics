dce237e502ad489ee145b758d0dfbb7889f81a30
UmamaheswararaoMeka <umamahesh_16@yahoo.co.in>
2017-05-23 09:18:33 +0530

    Adds FileSystem

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-function Person(firstName, lastname){
-	this.firstName = firstName;
-	this.lastname = lastname;
-}
+var fs = require("fs")
+var filepath = './sample.txt';
 
-var uma = new Person("Uma", "Mahesh");
+console.log("Start Reading File !!!!")
 
-console.log(uma);
+fs.readFileSync(filepath)
+fs.readFile(filepath, function(err, data){
+	if(err){
+		console.log(err)
+	}
+	console.log(data.toString())
+})
+
+console.log("Done Reading File !!!")
+
+console.log("Start writing file !!")
+
+fs.writeFile('./output.txt', 'Hello Techminds', function(err, data){
+	if(err){
+		throw err;
+		console.log(err)
+	}
+	console.log(data)
+})
+
+console.log("Done writing File !!!")
+
+var data = fs.readFileSync(filepath)
+fs.writeFileSync('uma.txt',data)
